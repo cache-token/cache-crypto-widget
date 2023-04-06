1008eb92f37ef08fdefa5b9be89d8777f380b821
vionjs <vionjs@gmail.com>
2023-04-06 16:32:30 +0700

    Update frontend

--- packages/frontend/src/models/Base.ts
+++ packages/frontend/src/models/Base.ts
@@ -5,12 +5,10 @@ export interface IAppConfig {
     RPC_URL: string;
     BLOCK_EXPLORER: string;
     ALCHEMY_API_KEY: string;
-    NATIVE: string;
   },
   CONTRACTS_ADDRESS: {
-    Wrapper: string;
     CGT: string;
     USDC: string;
-    WMATIC: string;
+    CGTSwap: string;
   }
 }
